8c63b4bb4caae9bab0b0b24254c76e5093cbec49
Vidiyam <131499411+15Maga@users.noreply.github.com>
2023-05-28 10:55:55 +0100

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o errexit  # exit on error
+
+pip install -r requirements.txt
+
+python3 -m pip install --upgrade pip
+
+python3 manage.py collectstatic --no-input
+
+python3 manage.py createsuperuser
+
+python3 manage.py makemigrations
+
+python3 manage.py migrate
